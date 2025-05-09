5f41360ee62e63212f0c8c003505d1bde644b42a
dev-M-Ali <dev.MuhammadAliButt@gmail.com>
2025-05-09 16:11:59 +0500

    Theme Persistence using LocalStorage Added

--- store/theme-context.js
+++ store/theme-context.js
@@ -1,4 +1,4 @@
-const { createContext, useState } = require("react");
+const { createContext, useState, useEffect } = require("react");
 import { ConfigProvider, theme } from 'antd';
 
 const ThemeContext = createContext({
@@ -13,12 +13,14 @@ export function ThemeProvider(props) {
     function enableLightMode() {
         document.body.style.background = 'white';
         setLightMode(true)
+        localStorage.setItem('theme', 'lightMode');
         //console.log("Switching to light mode")
     }
 
     function enableDarkMode() {
         document.body.style.background = 'black';
         setLightMode(false)
+        localStorage.setItem('theme', 'darkMode');
         //console.log("Switching to dark mode")
     }
 
@@ -27,6 +29,14 @@ export function ThemeProvider(props) {
         enableLightMode,
         enableDarkMode
     }
+
+    useEffect(() => {
+        const theme = localStorage.getItem('theme')
+
+        if (theme === "darkMode")
+        { enableDarkMode() }
+
+    }, [])
 
     //console.log("LightModeState's value is currently: ", lightModeState)
     return (
